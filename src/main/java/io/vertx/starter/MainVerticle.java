aecf3902bb70a8c7b8714231412a1e66472ae1b6
Christian Achilli <cachilli@avaya.com>
2018-10-12 09:08:45 +0100

    initial commit

---
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -0,0 +1,27 @@
+package io.vertx.starter;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.file.AsyncFile;
+import io.vertx.core.file.OpenOptions;
+import io.vertx.core.parsetools.RecordParser;
+
+public class MainVerticle extends AbstractVerticle {
+
+  @Override
+  public void start() {
+
+    AsyncFile asyncFile = vertx.fileSystem().openBlocking("sample.log", new OpenOptions().setRead(true).setWrite(false).setCreate(false));
+    RecordParser recordParser = RecordParser.newDelimited("\n", bufferedLine -> {
+        System.out.println("bufferedLine = " + bufferedLine);
+      });
+
+      asyncFile.handler(recordParser)
+          .endHandler(v -> {
+            asyncFile.close();
+            System.out.println("Done");
+          });
+  }
+
+
+
+}
