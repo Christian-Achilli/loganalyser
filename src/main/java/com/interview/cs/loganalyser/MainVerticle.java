8dee1b58e3596e4a6210b9727847723873882040
Christian Achilli <cachilli@avaya.com>
2018-10-13 17:11:07 +0100

    initial implementation

--- src/main/java/com/interview/cs/loganalyser/MainVerticle.java
+++ src/main/java/com/interview/cs/loganalyser/MainVerticle.java
@@ -9,10 +9,14 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.rxjava.core.AbstractVerticle;
 import io.vertx.rxjava.core.file.AsyncFile;
 import io.vertx.rxjava.core.parsetools.RecordParser;
+import io.vertx.rxjava.core.shareddata.LocalMap;
 import io.vertx.rxjava.ext.jdbc.JDBCClient;
 import io.vertx.rxjava.ext.sql.SQLConnection;
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -21,11 +25,12 @@ public class MainVerticle extends AbstractVerticle {
   private JsonObject config = new JsonObject()
     .put("url", "jdbc:hsqldb:mem:test?shutdown=true")
     .put("driver_class", "org.hsqldb.jdbcDriver")
-    .put("max_pool_size", 30);
+    .put("max_pool_size", 50)
+    .put("auto_commit_on_close", true);
   Gson gson = new Gson();
   private static final String INSERT_RAW_LOG = "insert into test values(?,?,?,?,?)";
   //first iteration, duration=timestamp and alert=state
-  private static final String CREATE_TABLE = "create table test(id varchar(10), duration bigint, type varchar(50), host varchar(20), alert varchar(10))";
+  private static final String CREATE_TABLE = "create table test( id varchar(10), duration bigint, type varchar(50), host varchar(20), alert varchar(10))";
   private static final String SELECT_BY_LOG_ID = "select * from test where id = ?";
 
   private JDBCClient client;
@@ -34,15 +39,18 @@ public class MainVerticle extends AbstractVerticle {
   @Override
   public void start(Future<Void> fut) {
 
+    AtomicInteger counter = new AtomicInteger();
+
     long started = System.currentTimeMillis();
 
-    AsyncFile asyncFile = vertx.fileSystem().openBlocking("sampleHUGE.log", new OpenOptions().setRead(true).setWrite(false).setCreate(false));
-
+    AsyncFile asyncFile = vertx.fileSystem().openBlocking("smallsample.log", new OpenOptions().setRead(true).setWrite(false).setCreate(false));
 
     client = JDBCClient.createShared(vertx, config);
 
     client.getConnection(conn -> {
-      ConcurrentHashMap<String, LogLine> tempMap = new ConcurrentHashMap<>();
+      LocalMap<String, String> tempMap = vertx.sharedData().getLocalMap("log-ids");
+      List<JsonArray> parametersList = new ArrayList<>();
+
       //System.out.println("TempMap size: "+tempMap.size());
       if (conn.failed()) {
         System.err.println(conn.cause().getMessage());
@@ -64,41 +72,38 @@ public class MainVerticle extends AbstractVerticle {
           } else {
 
             RecordParser recordParser = RecordParser.newDelimited("\n", data -> {
-              client.getConnection(conn2 -> {
-                //  System.out.println("Read data: " + data);
-                LogLine logLine = gson.fromJson(data.toJsonObject().toString(), LogLine.class);
-                //  System.out.println("LogLine id: "+ logLine.id);
-                LogLine logInMap = tempMap.get(logLine.id);
-                if (null == logInMap) {
-                  tempMap.put(logLine.id, logLine);
-                } else {
-                  //TODO handle error case when both state are the same (wrong log statement in the code)
-                  //TODO handle error case when transaction id are not unique
-                  long delta = Math.abs(logInMap.timestamp - logLine.timestamp);
-                  //System.out.println("id: " + logLine.id + " - delta: " + delta);
-                  String alert = "false";
-                  if (delta >= threshold) {
-                    alert = "true";
-                  }
 
-                  JsonArray params = new JsonArray().add(logLine.id).add(delta).add(logLine.type == null ? "" : logLine.type).add(logLine.host == null ? "" : logLine.host).add(alert);
-
-
-                  conn2.result().queryWithParams(INSERT_RAW_LOG, params, insertResult -> {
-                    if (insertResult.failed()) {
-                      System.err.println("Error inserting " + data.toString() + ": " + insertResult.cause());
-                      return;
-                    }
-                    //System.out.println("Removing log id from temp map: " + logInMap.id);
-                    tempMap.remove(logInMap.id);
-                    conn2.result().close();
-
-                  });
-
+              LogLine logLine = gson.fromJson(data.toJsonObject().toString(), LogLine.class);
+              System.out.println("LogLine id: " + logLine.id);
+              LogLine logInMap = gson.fromJson(tempMap.get(logLine.id), LogLine.class);
+              if (null == logInMap) {
+                tempMap.put(logLine.id, data.toJsonObject().toString());
+              } else {
+                //TODO handle error case when both state are the same (wrong log statement in the code)
+                //TODO handle error case when transaction id are not unique
+                long delta = Math.abs(logInMap.timestamp - logLine.timestamp);
+                //System.out.println("id: " + logLine.id + " - delta: " + delta);
+                String alert = "false";
+                if (delta >= threshold) {
+                  alert = "true";
                 }
 
+                JsonArray params = new JsonArray()
+                  .add(logLine.id)
+                  .add(delta)
+                  .add(logLine.type == null ? "" : logLine.type)
+                  .add(logLine.host == null ? "" : logLine.host)
+                  .add(alert);
 
-              });
+
+                parametersList.add(params);
+
+                if (counter.incrementAndGet() % 10000 == 0) {
+                  batchSave(counter, tempMap, parametersList, null);
+                } //end if counter
+
+
+              }
 
 
             });//end record parser
@@ -106,35 +111,65 @@ public class MainVerticle extends AbstractVerticle {
 
             asyncFile.handler(recordParser).endHandler(v -> {
               asyncFile.close();
-              System.out.println("Done inserting to DB");
-              System.out.println("TempMap size /1: " + tempMap.size());
 
-              connection.close(done -> {
-                System.out.println("DB Connection closed.");
-                System.out.println("TempMap size /2: " + tempMap.size());
-                System.out.println("Completed in :" + (System.currentTimeMillis() - started));
-                if (done.failed()) {
-                  throw new RuntimeException(done.cause());
-                }
-              });
+              Future endBatchSave = Future.future(n -> closeDown(counter, started, tempMap, connection));
+              if (parametersList.size() != 0) {
+                System.out.println("saving the last batch");
+                batchSave(counter, tempMap, parametersList, endBatchSave);
+              } else {
+                closeDown(counter, started, tempMap, connection);
+              }
 
-
-              /*connection.queryStream("select * from test", selectall -> {
-                selectall.result().handler(row -> {
-                  // System.out.println(row.encode());
-                }).endHandler(endOfSelect -> connection.close(done -> {
-                  System.out.println("DB Connection closed.");
-                  System.out.println("TempMap size /2: " + tempMap.size());
-                  System.out.println("Completed in :" + (System.currentTimeMillis() - started));
-                  if (done.failed()) {
-                    throw new RuntimeException(done.cause());
-                  }
-                }));*/
-              //});
             });//end async file handler
           }
         });
       });
+    });
+  }
+
+  private void closeDown(AtomicInteger counter, long started, LocalMap<String, String> tempMap, SQLConnection connection) {
+    System.out.println("Done inserting to DB");
+    System.out.println("TempMap size /1: " + tempMap.size());
+    AtomicInteger selectCounter = new AtomicInteger();
+    connection.queryStream("select * from test", selectall -> {
+      selectall.result().handler(row -> {
+        System.out.println(row.encode());
+        selectCounter.getAndIncrement();
+      }).endHandler(endOfSelect -> connection.close(done -> {
+        System.out.println("DB Connection closed.");
+        System.out.println("TempMap size /2: " + tempMap.size());
+        System.out.println("Completed in :" + (System.currentTimeMillis() - started));
+        System.out.println("Total rows to DB: " + selectCounter.get());
+        System.out.println("Total rows analyzed: " + counter.get());
+        if (done.failed()) {
+          throw new RuntimeException(done.cause());
+        }
+      }));
+    });
+  }
+
+  private void batchSave(AtomicInteger counter, LocalMap<String, String> tempMap, List<JsonArray> parametersList, Future endBatchSave) {
+
+    List<JsonArray> copyList = parametersList.stream().map(JsonArray::copy).collect(Collectors.toList());
+    System.out.println("Number of elements to batchsave: " + copyList.size());
+    System.out.println("Inserting to DB: " + counter.intValue() + " - parameters list size: " + copyList.size());
+    client.getConnection(conn2 -> {
+      SQLConnection batchConnection = conn2.result();
+      batchConnection.batchWithParams(INSERT_RAW_LOG, copyList, insertResult -> {
+        if (insertResult.failed()) {
+          System.err.println("Error inserting " + parametersList.size() + ": " + insertResult.cause());
+          insertResult.cause().printStackTrace();
+          return;
+        }
+        copyList.stream().forEach(jsonPar -> tempMap.remove(jsonPar.getString(0)));
+        parametersList.clear();
+        batchConnection.close();
+        System.out.println("Inserting to DB complete: " + counter.intValue() + " - parameters list size: " + copyList.size());
+        if (endBatchSave != null) {
+          endBatchSave.complete();
+        }
+      });
+
     });
   }
 
