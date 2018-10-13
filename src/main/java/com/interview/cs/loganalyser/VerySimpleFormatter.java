35398a6081dd304a5a0349fa386adea2aa28f2d4
Christian Achilli <cachilli@avaya.com>
2018-10-13 22:08:48 +0100

    added custom logger

---
+++ src/main/java/com/interview/cs/loganalyser/VerySimpleFormatter.java
@@ -0,0 +1,23 @@
+package com.interview.cs.loganalyser;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+/**
+ * To cusatomised log messages. It is used in the file vertx-default-jul-logging.properties, in src/main/resources
+ */
+public class VerySimpleFormatter extends Formatter {
+
+    private static final String PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
+
+    @Override
+    public String format(final LogRecord record) {
+        return String.format(
+                "%1$s %2$-7s %3$s %n",
+                new SimpleDateFormat(PATTERN).format(
+                        new Date(record.getMillis())),
+                record.getLevel().getName(), formatMessage(record));
+    }
+}
